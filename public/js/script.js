0e84ad51854d2d3d9fde182ffc04e70f87ca034a
Nikola Lukic <nlukic97@gmail.com>
2021-11-18 07:34:10 +0100

    reamde

--- public/js/script.js
+++ public/js/script.js
@@ -29,14 +29,15 @@ function connectToServer(token){
         
     });
     
+    // probably should just use http headers, not sure how to do it manually yet
     socket.on('202-connected',(token)=>{
-        console.log('Successfully authenticated to the server.', token);
+        console.log('Successfully authenticated to the server.');
         document.cookie = `token=${token}; expires=Sun, 1 Jan 2023 00:00:00 UTC; path=/`
         console.log(document.cookie);
     })
-
+    
     socket.on('redirect',(location)=>{
-        window.location.href = location
+        window.location.href = location // '/login' is the redirect location upon auth fail
     })
     
     socket.on('disconnect',()=>{
--- server.js
+++ server.js
@@ -7,30 +7,36 @@ const io = new Server(server);
 const port = 3000
 
 const path = require('path')
-app.use('/', express.static(path.join(__dirname, 'public'))) 
 
+// Paths
+app.use('/', express.static(path.join(__dirname, 'public'))) 
 app.use('/login', express.static(path.join(__dirname, 'public/login.html'))) 
 
 
-let conn = 0;
+let conn = 0; //nodes connected (if 2, do not allow other socket connections)
 
 io.on('connection', (socket) => {
   conn++
   console.log(socket.handshake.auth)
-
-  if(socket.handshake.auth.token != 'abc'){
-    socket.emit('redirect','/login')
-  } else {
-    console.log(socket.id + ' has connected');
-    socket.emit('202-connected',socket.handshake.auth.token)
-  }
-
+  Auth(socket)
   
   socket.on('disconnect', () => {
     conn--
     console.log(socket.id  + ' has disconnected');
   });
 });
+
+
+// Authentication method to the socket server (def not a propper auth, that could be done with this: passportjs.org/docs/downloads/html/   maybe)
+function Auth(socket){
+  if(socket.handshake.auth.token != 'abc'){
+    socket.emit('redirect','/login')
+    socket.disconnect() // just in case
+  } else {
+    console.log(socket.id + ' has connected');
+    socket.emit('202-connected',socket.handshake.auth.token)
+  }
+}
 
 
 server.listen(3000, () => {
