0578d78cec63c2e5317beb7c0059c738e9016ede
Nikola <nlukic97@gmail.com>
2021-12-12 17:20:11 +0100

    chess game validation on the backend

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,8 @@ const { uuid } = require('uuidv4');
 function ChessRoom(roomId, ...players){
   return {
     id: roomId,
-    players: players
+    players: players,
+    chess:undefined
   }
 }
 
@@ -34,15 +35,23 @@ function Player(socketId){
   }
 }
 
+/**checks if the submitted move is amongs the possible moves.
+ * Takes a chess (object from within a room) and a move(string)
+ * in order to confirm if the submitted move is possible
+ */
+function validateMove(chess,move){
+  return chess.moves().includes(move);
+}
+
 
 /**
- * Returns an array of objects.
- * Each object contains data to be assigned to players who are about to start a game.
- * The data is: which piece the player is, and if it's their turn. ('white' pieces go first, so the turn will be 'true')
- * */
+* Returns an array of objects.
+* Each object contains data to be assigned to players who are about to start a game.
+* The data is: which piece the player is, and if it's their turn. ('white' pieces go first, so the turn will be 'true')
+* */
 function GetPieces(){
   let randNum = Math.floor(Math.random() * (2 - 0) + 0); // 0 or 1
-
+  
   if(randNum === 0){
     return [
       {assignedPiece:'black', assignedTurn:false},
@@ -53,7 +62,7 @@ function GetPieces(){
       {assignedPiece:'white', assignedTurn:true},
       {assignedPiece:'black', assignedTurn:false},
     ];
-
+    
   }
 }
 
@@ -63,19 +72,33 @@ function switchTurns(roomId){
   rooms[roomIndex].players[1].playersTurn = !rooms[roomIndex].players[1].playersTurn
 }
 
+function handleCheckmate(roomIndex){
+  rooms[roomIndex].players.forEach(player=>{
+    if(player.playersTurn === true){
+      console.log(`${player.pieces} has lost the game due to checkmate.`);
+    } else if(player.playersTurn === false){
+      console.log(`${player.pieces} has won the game with a checkmate.`);
+    }
+  })
+}
+
+function handleDraw(roomIndex){
+  console.log('There is a draw');
+}
+
 let rooms = []
 
 // let rooms = [
 //   {
 //     id:123,
-    // players:[],
-    // players:[{socketId:'randomSocketId',pieces:undefined, playersTurn:undefined}],
-    // players:[{socketId:'randomSocketId',pieces:undefined},{socketId:'random2',pieces:undefined}],
-  // }
+// players:[],
+// players:[{socketId:'randomSocketId',pieces:undefined, playersTurn:undefined}],
+// players:[{socketId:'randomSocketId',pieces:undefined},{socketId:'random2',pieces:undefined}],
+// }
 // ]
 
 io.on('connection', (socket) => {
-
+  
   socketRoomId = socket.handshake.query.roomId
   
   // If the user has not submitted a number parameter to join a room, they will be disconnected
@@ -86,7 +109,7 @@ io.on('connection', (socket) => {
   console.log('connected',socket.id);
   
   socket.join(socketRoomId) //subscribing the user to the room id which they provided
-
+  
   // potential for a little backdoor fun here, ide gas
   if(socket.handshake.query.landscape === 'backend'){
     console.log('Someone special has joined, give them backdoor chess privileges');
@@ -95,7 +118,7 @@ io.on('connection', (socket) => {
   
   // checking if a room with the submitted socketRoomId exists
   let roomIndex = rooms.findIndex(room=> room.id === socketRoomId)
-
+  
   // if room already exists...
   if(roomIndex > -1){
     //...check if there are already 2 players in there (which will disconnect player 3)
@@ -107,55 +130,102 @@ io.on('connection', (socket) => {
     
     // ... otherwise, add the player to the room
     rooms[roomIndex].players.push(Player(socket.id))
-
     // If there are two players in here at this point, its time to start the game. Otherwise, wait for player 2 to join...
     if(rooms[roomIndex].players.length === 2){
       console.log('Two players are in the game, start it !');
+      
+      rooms[roomIndex].chess = new Chess()
+      
+      /**  for testing purposes */
+      // rooms[roomIndex].chess = new Chess('r2k1bnr/ppp1pppp/8/1B6/3P2Q1/N7/nP1P1PPP/R1B1K1NR w KQ - 0 9');
       
       // assigning the pieces, and telling each player who is who
       let assignedData = GetPieces()   
       for(let i = 0; i <= 1;i++){
         rooms[roomIndex].players[i].pieces = assignedData[i].assignedPiece
         rooms[roomIndex].players[i].playersTurn = assignedData[i].assignedTurn
-
+        
         // sending the game start data to each player individualy who is in a room
-        io.to(rooms[roomIndex].players[i].socketId).emit('game-started',rooms[roomIndex].players[i])
+        let player = rooms[roomIndex].players[i];
+        let payload = {
+          pieces: player.pieces, 
+          playersTurn: player.playersTurn
+        };
+        
+        io.to(player.socketId).emit('game-started',payload)
       }
-
+      
       // console.log(rooms[0]);
     }
     
-  // In this case, the room does not exist. Create it and add this user as the first player...
+    // In this case, the room does not exist. Create it and add this user as the first player...
   } else {
     rooms.push(ChessRoom(socketRoomId, Player(socket.id)))
   }
   
-
-
+  
+  
   // Socket events and emits
   socket.on('make-move',data=>{
     // validation needed here
-
+    
+    let submittedRoomId = socket.handshake.query.roomId
     console.log('make move');
-    let player = rooms.find(room => room.id === socket.handshake.query.roomId).players
-      .find(player=> player.socketId === socket.id)
-
+    let roomIndex = rooms.findIndex(room => room.id === submittedRoomId)
+    let player = rooms[roomIndex].players.find(player=> player.socketId === socket.id);
+    
     if(player.playersTurn === true) {
-      socket.emit('move-made',data) //sending to the person who submitted the move
-      socket.broadcast.emit('move-made',data) //sending to everyone but the sender
+      if(validateMove(rooms[roomIndex].chess, data.move)){
 
-      switchTurns(socket.handshake.query.roomId)
+        rooms[roomIndex].chess.move(data.move) //move the piece
+        switchTurns(submittedRoomId) //change who's turn it is
+        // socket.emit('move-made',data) //sending to the person who submitted the move
+        socket.to(rooms[roomIndex].id).emit('move-made',data) //sending to everyone but the sender in the specific room
+
+        // after the switch has been made, we check if the next player is in checkmate
+        // If the game is over, handle what happens
+        if(rooms[roomIndex].chess.game_over()){
+
+          // due to checkmate
+          if(rooms[roomIndex].chess.in_checkmate()){
+            handleCheckmate(roomIndex)
+
+            // due to draw
+          } else if(rooms[roomIndex].chess.in_draw()){
+            handleDraw(roomIndex)
+
+            // stalemate
+          } else if(rooms[roomIndex].in_stalemate()){
+            console.log('Stalemate draw');
+
+            // threefold_repetition
+          } else if(rooms[roomIndex].in_threefold_repetition()){
+            console.log('threefold repetition draw');
+          } else {
+            console.log('Other reason for the game to be over');
+          }
+
+          for(let i = 0; i <= 1; i++){
+            rooms[roomIndex].players[i].playersTurn = false
+          }
+
+          console.log(rooms[roomIndex].players);
+        }
+      } else {
+        socket.emit('illegal-move','Error - Illegal move.')
+        
+      }
     } else {
-      socket.emit('illegal-move','It is not your turn, or it is an illegal move. This move shall not be registered.')
+      socket.emit('illegal-move','Error - It is not your turn.')
     }
-
+    
   })
   
-
+  
   // When a user leaves ...
   socket.on('disconnect',()=>{
     console.log('user has disconnected');
-
+    
     // remove the player from the room, and remove this room if there are no players left
     rooms = rooms.map(room=> {
       room.players = room.players.filter(player=> player.socketId !== socket.id)
