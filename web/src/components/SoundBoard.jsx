71d61938cf753bb5f6c5bd2a0c43dc48d98aef84
Ljubisa Knezevic <ljubisakn@gmail.com>
2022-04-14 17:04:18 +0200

    added soundboard

---
+++ web/src/components/SoundBoard.jsx
@@ -0,0 +1,11 @@
+import Sound from "./Sound"
+
+const SoundBoard = () => (
+    <div>
+        <Sound event="dame-dameyu" source="dame-dameyu-short.mp3" text="dame-dameyu" />
+        <Sound event="kanye" source="wise words from kanye.mp3" text="wise words from kanye" />
+        <Sound event="uskliknimo" source="uskliknimo.mp3" text="uskliknimo" />
+    </div>
+)
+
+export default SoundBoard
